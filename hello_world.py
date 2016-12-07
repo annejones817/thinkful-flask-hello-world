0a18718a80e14b1b238cf2b5d373d2412f088b3f
Anne Jones <annejones817@gmail.com>
2016-12-07 19:01:41 +0000

    Updated to use Jinja rather than Flask string formatting

--- hello_world.py
+++ hello_world.py
@@ -1,26 +1,17 @@
-from flask import Flask
-from os import environ
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/hello")
 def say_hi(): 
-    return "Hello World!"
+    return render_template('base.html')
 
 @app.route("/hello/<name>")
 def hi_person(name):
-    #Why do you have to put the three quotes? 
-    html = """
-        <h1>
-        Hello {}!
-        </h1>
-        <p>
-        Here's a picture of a kitten. Aww...
-        </p>
-        <img src = "http://placekitten.com/g/200/300" />
-    """
-    return html.format(name.title())
+    return render_template('template.html', 
+                            name = name.title())
     
 @app.route("/jedi/<first>/<last>")
 def jedi(first, last): 
@@ -36,8 +27,8 @@ def jedi(first, last):
             jediName += first[m]
     else: 
        jediName += 'xx'
-    return 'Your jedi name is {}.'.format(jediName.title())   
+    return render_template('jedi.html',
+                            jediName = jediName.title())
     
 if __name__ == "__main__":
-    app.run(host=environ['IP'], 
-            port=int(environ['PORT']))
+    app.run(debug=True, host="0.0.0.0", port=8080)
